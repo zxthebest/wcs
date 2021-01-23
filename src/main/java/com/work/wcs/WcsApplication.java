754bfb4f14e902818cf03f4550d77fd35fb58908
361168813 <361168813@qq.com>
2021-01-23 23:25:03 +0800

    Initial commit

---
+++ src/main/java/com/work/wcs/WcsApplication.java
@@ -0,0 +1,13 @@
+package com.work.wcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/work/wcs/WcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.wcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
